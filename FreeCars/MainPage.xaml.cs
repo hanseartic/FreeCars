e60be6acc2802c3acdcd1105fbe6a5d953bfa6e4
Paul Schütte <p.schuette@plexgroup.com>
2012-10-09 15:22:02 +0200

    Add show-my-location functionality and reload at application start.

--- FreeCars/MainPage.xaml.cs
+++ FreeCars/MainPage.xaml.cs
@@ -15,98 +15,103 @@ using System.Device.Location;
 using System.Globalization;
 using System.Windows.Data;
 using Microsoft.Phone.Shell;
+using System.IO.IsolatedStorage;
 
 namespace FreeCars {
-    public partial class MainPage : PhoneApplicationPage {
-        // Constructor
-        private Pushpin me;
-        private bool trackMyLocation = false;
+		public partial class MainPage : PhoneApplicationPage {
 				private GeoCoordinateWatcher cw;
-        public MainPage() {
-            InitializeComponent();
-            ((App)App.Current).CarsLoaded += OnCarsLoaded;
-            cw = new GeoCoordinateWatcher(GeoPositionAccuracy.High);
-            cw.MovementThreshold = 20;
-            cw.PositionChanged += OnMyPositionChanged;
-            me = new Pushpin() { 
-                Opacity = .6,
-                Content = "You",
-                Background = (Brush)Application.Current.Resources["PhoneAccentBrush"],
-            };
-            cw.Start();
-            map.Children.Add(me);
-						
+				public MainPage() {
+						InitializeComponent();
+						((App)App.Current).CarsLoaded += OnCarsLoaded;
+						cw = new GeoCoordinateWatcher(GeoPositionAccuracy.High);
+						cw.MovementThreshold = 10;
+						cw.PositionChanged += OnMyPositionChanged;
+						Loaded += OnMainPageLoaded;
+						//map.Children.Add(me);
 						SetAppBar();
-        }
+				}
 
 				void OnMainPageLoaded(object sender, RoutedEventArgs e) {
-						
+						try {
+								var position = (GeoPosition<GeoCoordinate>)IsolatedStorageSettings.ApplicationSettings["my_last_location"];
+								ShowMeAtLocation(position.Location);
+						} catch (KeyNotFoundException) {}
 				}
 				void SetAppBar() {
-						ApplicationBar = new ApplicationBar();
+						ApplicationBar = new ApplicationBar {
+								Mode = ApplicationBarMode.Default,
+								Opacity = 1.0,
+								IsVisible = true,
+								IsMenuEnabled = true,
+						};
 
-						ApplicationBar.Mode = ApplicationBarMode.Default;
-						ApplicationBar.Opacity = 1.0;
-						ApplicationBar.IsVisible = true;
-						ApplicationBar.IsMenuEnabled = true;
+						var mainPageApplicationBarCentermeButton = new ApplicationBarIconButton {
+								IconUri = new Uri("/Resources/appbar.map.centerme.rest.png", UriKind.Relative),
+								Text = FreeCars.Resources.Strings.MainpageAppbarCenterme,
+						};
+						mainPageApplicationBarCentermeButton.Click += OnMainPageApplicationBarCentermeButtonClick;
 
-						var mainPageApplicationBarReloadButton = new ApplicationBarIconButton();
-						mainPageApplicationBarReloadButton.IconUri = new Uri("/Resources/dark_appbar.feature.settings.rest.png", UriKind.Relative);
-						mainPageApplicationBarReloadButton.Text = FreeCars.Resources.Strings.MainPageBarSettings;
-						mainPageApplicationBarReloadButton.Click += OnMainPageApplicationBarReloadButtonClick;
-
-						var mainPageApplicationBarSettingsButton = new ApplicationBarIconButton();
-						mainPageApplicationBarSettingsButton.IconUri = new Uri("/Resources/dark_appbar.feature.settings.rest.png", UriKind.Relative);
-						mainPageApplicationBarSettingsButton.Text = FreeCars.Resources.Strings.MainPageBarMyLocation;
+						var mainPageApplicationBarSettingsButton = new ApplicationBarIconButton {
+								IconUri = new Uri("/Resources/dark_appbar.feature.settings.rest.png", UriKind.Relative),
+								Text = FreeCars.Resources.Strings.MainPageBarSettings,
+						};
 						mainPageApplicationBarSettingsButton.Click += OnMainPageApplicationBarSettingsButtonClick;
 
-						ApplicationBar.Buttons.Add(mainPageApplicationBarReloadButton);
+						ApplicationBar.Buttons.Add(mainPageApplicationBarCentermeButton);
 						ApplicationBar.Buttons.Add(mainPageApplicationBarSettingsButton);
 				}
 
-				void OnMainPageApplicationBarReloadButtonClick(object sender, EventArgs e) {
-						
-				}
-				void OnMainPageApplicationBarSettingsButtonClick(object sender, EventArgs e) {
-						trackMyLocation = true;
+				void OnMainPageApplicationBarCentermeButtonClick(object sender, EventArgs e) {
+						myLocationPushpin.Visibility = Visibility.Collapsed;
 						cw.Start();
 				}
-        void OnMyPositionChanged(object sender, GeoPositionChangedEventArgs<GeoCoordinate> e) {
-            me.Location = e.Position.Location;
-						if (true == trackMyLocation) {
-								map.Center = e.Position.Location;
-								map.ZoomLevel = 14.0;
-						} else {
-								cw.Stop();
+				void OnMainPageApplicationBarSettingsButtonClick(object sender, EventArgs e) {
+
+				}
+				void OnMyPositionChanged(object sender, GeoPositionChangedEventArgs<GeoCoordinate> e) {
+						if (GeoPositionStatus.Ready != ((GeoCoordinateWatcher)sender).Status) return;
+						cw.Stop();
+						try {
+								IsolatedStorageSettings.ApplicationSettings.Add("my_last_location", e.Position);
+						} catch (ArgumentException) {
+								IsolatedStorageSettings.ApplicationSettings["my_last_location"] = e.Position;
 						}
-        }
+						IsolatedStorageSettings.ApplicationSettings.Save();
+						ShowMeAtLocation(e.Position.Location);
+				}
+				void ShowMeAtLocation(GeoCoordinate location) {
+						myLocationPushpin.Location = location;
+						map.Center = location;
+						map.ZoomLevel = 14.0;
+						myLocationPushpin.Visibility = Visibility.Visible;
+				}
 
-        void OnCarsLoaded(object sender, EventArgs e) {
-            var cultureInfo = new CultureInfo("en-US");
-            foreach (var car in ((App)sender).Cars) {
-                var coordinate = new GeoCoordinate(
-                    Double.Parse(car.lat, cultureInfo.NumberFormat),
-                    Double.Parse(car.lng, cultureInfo.NumberFormat));
+				void OnCarsLoaded(object sender, EventArgs e) {
+						var cultureInfo = new CultureInfo("en-US");
+						foreach (var car in ((App)sender).Cars) {
+								var coordinate = new GeoCoordinate(
+										Double.Parse(car.lat, cultureInfo.NumberFormat),
+										Double.Parse(car.lng, cultureInfo.NumberFormat));
 
-                var pushpin = new Pushpin() {
-                    Location = coordinate,
-                    Name = car.hal2option.tooltip,
-                    Opacity = .3,
-                    Width = 20
+								var pushpin = new Pushpin() {
+										Location = coordinate,
+										Name = car.hal2option.tooltip,
+										Opacity = .3,
+										Width = 20
 
-                };
-                pushpin.Tap += OnPushpinTap;
-                map.Children.Add(pushpin);
-                //pushpin.Location = 
-            }  
-        }
+								};
+								pushpin.Tap += OnPushpinTap;
+								map.Children.Add(pushpin);
+								//pushpin.Location = 
+						}  
+				}
 
-        void OnPushpinTap(object sender, GestureEventArgs e) {
-            var provider = "Multicity";
-            MessageBox.Show(((Pushpin)sender).Name.Replace("'", ""), provider, MessageBoxButton.OK);
-        }
-        public void LoadFreeCars() {
-            
-        }
-    }
+				void OnPushpinTap(object sender, GestureEventArgs e) {
+						var provider = "Multicity";
+						MessageBox.Show(((Pushpin)sender).Name.Replace("'", ""), provider, MessageBoxButton.OK);
+				}
+				public void LoadFreeCars() {
+						
+				}
+		}
 }
