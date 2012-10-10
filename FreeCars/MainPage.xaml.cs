096b206e3b9ce06ba7573cb7a9456b008d75c4ab
hanseartic <schuette.paul@gmail.com>
2012-10-10 16:27:12 +0200

    Add more information to the Pushpins
    * Show basic information in Opening Pushpin instead of modal dialog.
    * Keep opened Pushpin on top.

--- FreeCars/MainPage.xaml.cs
+++ FreeCars/MainPage.xaml.cs
@@ -116,11 +116,9 @@ namespace FreeCars {
 				}
 
 				void OnCarsUpdated(object sender, EventArgs e) {
-						
-                        if (sender.GetType() == typeof(Multicity)) { 
-                            UpdateMulticityLayers();
-                        }
-                        
+						if (sender.GetType() == typeof(Multicity)) {
+								UpdateMulticityLayers((Multicity)sender);
+						}
 						if (sender.GetType() == typeof(DriveNow)) {
 								UpdateDriveNowLayer((DriveNow)sender);
 						}                
@@ -148,51 +146,101 @@ namespace FreeCars {
 								driveNowCarsLayer.Children.Add(pushpin);
 						}
 				}
-                void UpdateMulticityLayers() {
-                    var myLocation = myLocationPushpin.Location;
-                    var multicity = (Multicity)((App)Application.Current).Resources["multicity"];
-                    var cultureInfo = new CultureInfo("en-US");
-                    var multcityCarsBrush = new SolidColorBrush(Colors.Red);
-                    var multcityChargersBrush = new SolidColorBrush(Colors.Blue);
-                    multicityCarsLayer.Children.Clear();
-                    var tempList = new List<Pushpin>();
-                    foreach (var car in multicity.MulticityCars) {
-                        var coordinate = new GeoCoordinate(
-                                        Double.Parse(car.lat, cultureInfo.NumberFormat),
-                                        Double.Parse(car.lng, cultureInfo.NumberFormat));
-                        var distance = (int)coordinate.GetDistanceTo(myLocation);
-                        if (1500 < distance) continue;
-                        var pushpin = new Pushpin() {
-                            Location = coordinate,
-                            Name = car.hal2option.tooltip,
-                            Opacity = .6,
-                            Background = multcityCarsBrush,
-                            Content = distance + " m",
-                        };
-                        pushpin.Tap += OnPushpinTap;
-                        multicityCarsLayer.Children.Add(pushpin);
-                    }
-                    multicityFlinksterLayer.Children.Clear();
-                    multicityChargingLayer.Children.Clear();
-                    foreach (var station in multicity.MulticityChargers) {
-                        var coordinate = new GeoCoordinate(
-                            Double.Parse(station.lat, cultureInfo.NumberFormat),
-                            Double.Parse(station.lng, cultureInfo.NumberFormat));
-                        var pushpin = new Pushpin() {
-                            Location = coordinate,
-                            Name = station.hal2option.tooltip,
-                            Opacity = .6,
-                            Background = multcityChargersBrush,
-                        };
-                    }
+				void UpdateMulticityLayers(Multicity multicity) {
+						var myLocation = myLocationPushpin.Location;
+						var cultureInfo = new CultureInfo("en-US");
+						var multcityCarsBrush = new SolidColorBrush(Colors.Red);
+						var multcityChargersBrush = new SolidColorBrush(Colors.Blue);
+						multicityCarsLayer.Children.Clear();
+						var tempList = new List<Pushpin>();
+						foreach (var car in multicity.MulticityCars) {
+								try {
+										var coordinate = new GeoCoordinate(
+																		Double.Parse(car.lat, cultureInfo.NumberFormat),
+																		Double.Parse(car.lng, cultureInfo.NumberFormat));
+										var distance = (int)coordinate.GetDistanceTo(myLocation);
+										if (1500 < distance) continue;
+										var pushpinContent = new Border {
+												Child = new StackPanel {
+														Orientation = System.Windows.Controls.Orientation.Vertical,
+														Children = {
+														new TextBlock { Text = Strings.Multicity, },
+														new TextBlock { Text = car.hal2option.tooltip, },
+																
+														},
+												},
+												Visibility = Visibility.Collapsed,
+										};
+										var pushpin = new Pushpin() {
+												Location = coordinate,
+												Name = car.hal2option.tooltip,
+												Opacity = .6,
+												Background = multcityCarsBrush,
+												Content = pushpinContent,
+												//Content = distance + " m",
+										};
+										pushpin.Tap += OnPushpinTap;
+										multicityCarsLayer.Children.Add(pushpin);
+								} catch (ArgumentException) { }
+						}
+						multicityFlinksterLayer.Children.Clear();
+						multicityChargingLayer.Children.Clear();
+						foreach (var station in multicity.MulticityChargers) {
+								var coordinate = new GeoCoordinate(
+										Double.Parse(station.lat, cultureInfo.NumberFormat),
+										Double.Parse(station.lng, cultureInfo.NumberFormat));
+								var pushpin = new Pushpin() {
+										Location = coordinate,
+										Name = station.hal2option.tooltip,
+										Opacity = .6,
+										Background = multcityChargersBrush,
+								};
+						}
 
-                }
+				}
 				void OnPushpinTap(object sender, System.Windows.Input.GestureEventArgs e) {
 						var provider = "Multicity";
-						MessageBox.Show(((Pushpin)sender).Name.Replace("'", ""), provider, MessageBoxButton.OK);
+						var pushpinContent = ((Pushpin)sender).Content;
+						
+						if (typeof(Border) == pushpinContent.GetType()) {
+								if (Visibility.Collapsed == ((Border)pushpinContent).Visibility) {
+										foreach (var pushpin in activeLayer.Children.ToArray()) {
+												if (pushpin.GetType() == typeof(Pushpin)) {
+														DeactivatePushpin(pushpin as Pushpin);
+												}
+										}
+										var parentLayer = VisualTreeHelper.GetParent((Pushpin)sender) as MapLayer;
+										parentLayer.Children.Remove((Pushpin)sender);
+										activeLayer.Children.Add((Pushpin)sender);
+										((Pushpin)sender).Tag = parentLayer;
+										((Pushpin)sender).Opacity = 1;
+										((Border)pushpinContent).Visibility = Visibility.Visible;
+
+								} else {
+										DeactivatePushpin(sender as Pushpin);
+								}
+						}
+						e.Handled = true;
+				}
+				void DeactivatePushpin(Pushpin pushpin) {
+						if (null != pushpin.Tag && typeof(MapLayer) == pushpin.Tag.GetType()) {
+								activeLayer.Children.Remove(pushpin);
+								((MapLayer)pushpin.Tag).Children.Add(pushpin);
+								pushpin.Tag = null;
+						}
+						((Border)pushpin.Content).Visibility = Visibility.Collapsed;
+						pushpin.Opacity = .6;
 				}
 				public void LoadFreeCars() {
 						
+				}
+
+				private void OnMapTap(object sender, System.Windows.Input.GestureEventArgs e) {
+						foreach (var pushpin in activeLayer.Children.ToArray()) {
+								if (pushpin.GetType() == typeof(Pushpin)) {
+										DeactivatePushpin(pushpin as Pushpin);
+								}
+						}
 				}
 		}
 }
