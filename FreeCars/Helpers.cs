2681b207d9d03c006d8aa483f2d91bc40e0fc950
hanseartic <schuette.paul@gmail.com>
2013-02-02 22:29:30 +0100

    Add method to make a POST-request

--- FreeCars/Helpers.cs
+++ FreeCars/Helpers.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 
 namespace FreeCars {
@@ -13,6 +14,33 @@ namespace FreeCars {
 			writer.Flush();
 			stream.Position = 0;
 			return stream;
+		}
+
+		public static void Post(string address, string parameters, Action<Stream> onResponseGot) {
+			Uri uri = new Uri(address, UriKind.Absolute);
+
+			HttpWebRequest r = (HttpWebRequest)WebRequest.Create(uri);
+			r.Method = "POST";
+			r.ContentType = "application/x-www-form-urlencoded";
+
+			r.BeginGetRequestStream(delegate(IAsyncResult req) {
+					var outStream = r.EndGetRequestStream(req);
+					using (StreamWriter w = new StreamWriter(outStream))
+						w.Write(parameters);
+					r.BeginGetResponse(delegate(IAsyncResult result) {
+							try {
+								HttpWebResponse response = (HttpWebResponse)r.EndGetResponse(result);
+								using (var stream = response.GetResponseStream()) {
+									onResponseGot(stream);
+									//using (StreamReader reader = new StreamReader(stream)) {
+									//	onResponseGot(reader.ReadToEnd());
+									//}
+								}
+							} catch (WebException ex) {
+								onResponseGot(null);
+							}
+						}, null);
+				}, null);
 		}
 	}
 }
