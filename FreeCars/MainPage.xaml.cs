ca3eb20d25114161f3f6fce16e941162a7dda1ee
hanseartic <schuette.paul@gmail.com>
2013-08-11 12:20:00 +0200

    Prepares loading indicator.

--- FreeCars/MainPage.xaml.cs
+++ FreeCars/MainPage.xaml.cs
@@ -110,11 +110,21 @@ namespace FreeCars {
 				e.Cancel = true;
 			}
 		}
+
+		private void ReloadPOIs() {
+			isRefreshingCar2Go = true;
+			isRefreshingDriveNow = true;
+			isRefreshingMulticity = true;
+			VisualStateManager.GoToState(this, "CarsLoadingShow", true);
+			VisualStateManager.GoToState(this, "isRefreshingCar2Go", false);
+			VisualStateManager.GoToState(this, "isRefreshingDriveNow", false);
+			VisualStateManager.GoToState(this, "isRefreshingMulticity", false);
+			((App)Application.Current).ReloadPOIs();
+		}
 		protected override void OnNavigatedTo(NavigationEventArgs e) {
 			base.OnNavigatedTo(e);
 			CheckTrialAndAds();
-			((App)Application.Current).ReloadPOIs();
-
+			ReloadPOIs();
 			try {
 				gpsAllowed = (bool)IsolatedStorageSettings.ApplicationSettings["settings_use_GPS"];
 			} catch (KeyNotFoundException) {
@@ -217,7 +227,7 @@ namespace FreeCars {
 			bookingControl.Closed += (sender, args) => { ApplicationBar.IsVisible = true; };
 			bookingControl.ActionCompleted += (sender, args) => {
 				DeactivateAllPushpins();
-				((App)Application.Current).ReloadPOIs();
+				ReloadPOIs();
 			};
 		}
 
@@ -240,7 +250,7 @@ namespace FreeCars {
 			NavigationService.Navigate(new Uri("/SettingsPage.xaml", UriKind.RelativeOrAbsolute));
 		}
 		void OnMainPageApplicationBarReloadButtonClick(object sender, EventArgs e) {
-			((App)Application.Current).ReloadPOIs();
+			ReloadPOIs();
 		}
 		private void OnMainPageApplicationBarPinButtonClick(object sender, EventArgs e) {
 			pinCurrentMapCenterAsSecondaryTile();
@@ -269,16 +279,26 @@ namespace FreeCars {
 			myLocationPushpin.Visibility = Visibility.Visible;
 		}
 
+		private bool isRefreshingMulticity, isRefreshingDriveNow, isRefreshingCar2Go;
 		void OnCarsUpdated(object sender, EventArgs e) {
 			if (sender.GetType() == typeof(Multicity)) {
 				UpdateMulticityLayers((Multicity)sender);
+				isRefreshingMulticity = false;
+				VisualStateManager.GoToState(this, "isRefreshingMulticityDone", true);
 			}
 			if (sender.GetType() == typeof(DriveNow)) {
 				UpdateDriveNowLayer((DriveNow)sender);
+				isRefreshingDriveNow = false;
+				VisualStateManager.GoToState(this, "isRefreshingDriveNowDone", true);
 			}
 			if (sender.GetType() == typeof(Car2Go)) {
 				UpdateCar2GoLayer((Car2Go)sender);
+				isRefreshingCar2Go = false;
+				VisualStateManager.GoToState(this, "isRefreshingCar2GoDone", true);
 			}
+			if (isRefreshingCar2Go || isRefreshingDriveNow || isRefreshingMulticity)
+				return;
+			VisualStateManager.GoToState(this, "CarsLoadingHide", true);
 		}
 		void UpdateDriveNowLayer(DriveNow driveNow) {
 			var centerLocation = map.Center;
